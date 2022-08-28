ba5f48512547cb6a550813afab3c4e5dba4b26ec
DeKabilan <38588004+DeKabilan@users.noreply.github.com>
2022-08-28 20:31:16 +0530

    HI

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const tabs = document.querySelectorAll('#top-tabs > div')
 
 function hideOtherContentAreas(){
     const allContentAreas = document.querySelectorAll('#content-area > div')
+
     for(let i=0;i<allContentAreas.length;i++){
         allContentAreas[i].style.display = 'none'
     }
 }
+
 
 function changeTab(){
     const selector = this.dataset.activate
