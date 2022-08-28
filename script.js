30a505bc9b2f96f22949c680146019aa2733fd4e
DeKabilan <38588004+DeKabilan@users.noreply.github.com>
2022-08-28 19:14:50 +0530

    Help Poo

---
+++ script.js
@@ -0,0 +1,25 @@
+const tabs = document.querySelectorAll('#top-tabs > div')
+
+function hideOtherContentAreas(){
+    const allContentAreas = document.querySelectorAll('#content-area > div')
+    for(let i=0;i<allContentAreas.length;i++){
+        allContentAreas[i].style.display = 'none'
+        console.log(this)
+    }
+}
+
+function changeTab(){
+    const selector = this.dataset.activate
+    if(selector){
+        const el = document.querySelector(selector)
+        hideOtherContentAreas()
+        el.style.display = "block"
+        console.log(this)
+    }
+}
+
+for (let i=0;i<tabs.length;i++ ){
+    const tab = tabs[i]
+    tab.addEventListener('click', changeTab, false)
+}
+
